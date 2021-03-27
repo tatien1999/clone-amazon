68dfe35005a3a4dbb6a2b5b98fc7890e24831e00
tatien1999 <tatien1999@gmail.com>
2021-03-27 15:55:04 +0700

    cloneamazon-firebase

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react'
+import './App.css';
+import Header from './Header'
+import Cart from './Cart'
+import Home from './Home'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import styled from 'styled-components'
+import { db } from './firebase'
+
+function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const getCartItems = () => {
+    db.collection('cartItems').onSnapshot((snapshot) => {
+      const tempItems = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        product: doc.data()
+      }))
+
+      setCartItems(tempItems);
+    })
+  }
+
+  useEffect(() => {
+    getCartItems();
+  }, [])
+
+  return (
+    <Router>
+      <Container>
+        <Header />
+
+        <Switch>
+
+          <Route path="/cart">
+            <Cart cartItems={cartItems} />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+
+        </Switch>
+      </Container>
+    </Router>
+  );
+}
+
+export default App;
+
+const Container = styled.div`
+  background-color: #EAEDED;
+`
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyCGX_THJw70QIOOQz_48V9iaqsCCXnB_JA",
+  authDomain: "daychallenge-702c2.firebaseapp.com",
+  projectId: "daychallenge-702c2",
+  storageBucket: "daychallenge-702c2.appspot.com",
+  messagingSenderId: "329055837238",
+  appId: "1:329055837238:web:c70ecc45eada4e4ede0c97",
+  measurementId: "G-43S09G51MT"
+});
+
+const db = firebase.firestore();
+
+export { db }
